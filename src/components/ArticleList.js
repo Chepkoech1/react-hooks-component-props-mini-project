95333e7489361864cff617b1cb33a0ec520b8b09
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-01-20 15:08:41 +0300

    done

---
+++ src/components/ArticleList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import blogData from "../data/blog";
+import Article from "./Article";
+
+
+function ArticleList(){
+    return(
+        <main>
+            <Article/>
+        </main>
+    )
+}
+export default ArticleList
